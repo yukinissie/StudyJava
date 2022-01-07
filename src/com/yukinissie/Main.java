7262ec423690dfc22ea6d9376ae80ec6771abdfb
yukinissie <bigtree2490@outlook.jp>
2022-01-08 03:15:46 +0900

    FizzBuzz

--- src/com/yukinissie/Main.java
+++ src/com/yukinissie/Main.java
@@ -3,6 +3,16 @@ package com.yukinissie;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java again!");
+        for (int i = 1; i <= 100; i ++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
